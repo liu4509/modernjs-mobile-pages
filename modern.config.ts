96522fb3f04049bb7262e200ed602cab7614d435
zhibin <2608606700@qq.com>
2023-02-01 18:50:03 +0800

    关闭客户端路由和状态管理

--- modern.config.ts
+++ modern.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@modern-js/app-tools';
 // https://modernjs.dev/docs/apis/app/config
 export default defineConfig({
   runtime: {
-    router: true,
-    state: true,
+    router: false,
+    state: false,
   },
 });
